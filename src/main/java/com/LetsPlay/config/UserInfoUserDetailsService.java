a53abf34377027a48e06b98e1425c7513e82e498
Dang Lam <dang.lam@gritlab.ax>
2023-08-26 18:42:30 +0300

    more updates

---
+++ src/main/java/com/LetsPlay/config/UserInfoUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.LetsPlay.config;
+
+import com.LetsPlay.model.User;
+import com.LetsPlay.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UserInfoUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(username);
+        return user.map(UserInfoUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + username + " not found "));
+    }
+}
